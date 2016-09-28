b2856dbf6d9572918d8f083bf7c8dab8d34a751c
Gareth Valentin <GV@Gareths-MacBook-Pro.local>
2016-09-28 04:20:51 -0400

    Initial commit

---
+++ app/src/main/java/edu/bard/todolist_lab1/ItemView.java
@@ -0,0 +1,52 @@
+package edu.bard.todolist_lab1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class ItemView extends Fragment {
+
+    private ToDoList mToDoList;
+    private EditText mEditText;
+    private Button mButton;
+    public Listener mListener;
+
+
+    public interface Listener {
+        public void itemToSend(String s);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mToDoList = new ToDoList();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_item_view, container, false);
+
+        mEditText = (EditText)v.findViewById(R.id.crime_title);
+
+
+        mButton = (Button)v.findViewById(R.id.addButton);
+        mButton.setEnabled(true);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mListener.itemToSend(mEditText.getText().toString());
+                mEditText.setText("");
+            }
+        });
+
+        return v;
+    }
+
+}
