402fd7680f3175827d3cbe1e9a1855d8ac371ec9
Sven Anderson <sveneanderson@gmail.com>
2016-10-24 18:58:57 -0400

    HW #2 feedback

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -1,5 +1,49 @@
 package edu.bard.todolist_lab1;
+/*
 
+
+CMSC 374
+HW #2 Fragments: Feedback
+
+Names: Dan, Gareth, and ???? (let me know)
+
+     Rubric
+----------------------------------------------------------
+40	1. App functions correctly from end user perspective.
+20	2. Fragments are correctly created.
+10	3. Fragments share information on the adapter arraylist.
+5	4. Code is clearly commented.
+20	5. Code is readable and well constructed.
+
+Score: 95
+
+
+----------------
+
+Comments
+
+My comments in your code are marked with XX in a comment.
+
+
+
+1. App functions correctly from end user perspective. YES, resources used, too.
+
+2. Fragments are correctly created.
+
+3. Fragments share information on the adapter arraylist. Yes, you
+share via a method in main activity.
+
+4. Code is clearly commented.
+You ignore commenting important methods.
+
+
+5. Code is readable and well constructed.
+Yes...good throughout.
+
+
+
+
+ */
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,7 +66,7 @@ public class ToDoList extends FragmentActivity implements ItemView.ItemListener{
 
 
         FragmentManager fm = getSupportFragmentManager();
-
+// XX this part is ok
         Fragment fragment1 = fm.findFragmentById(R.id.Listview);
         if (fragment1 == null) {
             fragment1 = new ListViewer();
@@ -42,7 +86,8 @@ public class ToDoList extends FragmentActivity implements ItemView.ItemListener{
         Log.i(TAG, "Entered onCreate");
     }
 
-    public void itemToSend (String item) {
+    // XX this method requires documentation
+       public void itemToSend (String item) {
 
         mItem = item;
         ListViewer Pac = (ListViewer) getSupportFragmentManager().findFragmentById(R.id.Listview);
